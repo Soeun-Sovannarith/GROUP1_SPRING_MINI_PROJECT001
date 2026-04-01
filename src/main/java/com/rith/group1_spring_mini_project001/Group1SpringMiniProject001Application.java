8221db10842b8bbafe0c4d3f65eed8149e6486b7
RithCB <seounsovanarith@gmail.com>
2026-04-01 17:04:30 +0700

    first commit

---
+++ src/main/java/com/rith/group1_spring_mini_project001/Group1SpringMiniProject001Application.java
@@ -0,0 +1,13 @@
+package com.rith.group1_spring_mini_project001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group1SpringMiniProject001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group1SpringMiniProject001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/rith/group1_spring_mini_project001/Group1SpringMiniProject001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rith.group1_spring_mini_project001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group1SpringMiniProject001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
