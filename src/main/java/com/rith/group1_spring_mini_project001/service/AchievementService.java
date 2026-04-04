0435d2d8235745fdb8c6e4c052b78963018b9c3d
Soeun-Sovannarith <soeunsovannarith64@gmail.com>
2026-04-04 14:23:01 +0700

    Achievement finish

--- src/main/java/com/rith/group1_spring_mini_project001/service/AchievementService.java
+++ src/main/java/com/rith/group1_spring_mini_project001/service/AchievementService.java
@@ -4,10 +4,11 @@ import com.rith.group1_spring_mini_project001.model.model.Achievement;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public interface AchievementService {
     List<Achievement> getAchievements(Integer page, Integer size);
 
-    List<Achievement> getAchievementsCurrentUser(Integer page, Integer size);
+    List<Achievement> getAchievementsCurrentUser(UUID userId, Integer page, Integer size);
 }
