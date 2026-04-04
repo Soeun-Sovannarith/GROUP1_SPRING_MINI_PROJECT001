73270fa1813fc18f3ea0476d9575ffc5320cddb8
Soeun-Sovannarith <soeunsovannarith64@gmail.com>
2026-04-04 09:49:33 +0700

    change credentail to .env

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO achievements (achievement_id, title, description, badge, xp_required) VALUES
+                                                                                      ('a1ea5bc1-dfb8-4b02-8394-4ecf8441cfe5', 'First Habit Completed', 'Awarded when a user completes their first habit.', 'first_habit_badge.png', 50),
+
+                                                                                      ('d9c736f7-8b86-4bfb-9a25-ca1fc057ae10', '7-Day Streak', 'Awarded for completing a habit for 7 consecutive days.', '7_day_streak_badge.png', 100),
+
+                                                                                      ('a2daf8ee-516b-4487-807d-5ea5314a8cb0', '30-Day Streak', 'Awarded for completing a habit for 30 consecutive days.', '30_day_streak_badge.png', 200),
+
+                                                                                      ('3266a8eb-b73c-4137-800c-a4c3db2313f4', 'Habit Master', 'Awarded for completing 10 different habits.', 'habit_master_badge.png', 500),
+
+                                                                                      ('1c4dd9ec-47ca-4407-87ad-3b39f1de9fb2', 'Perfect Month', 'Awarded for completing a habit every day in a given month.', 'perfect_month_badge.png', 300),
+
+                                                                                      ('f60a2b80-577c-4a6f-b8d8-34c3d85b6283', 'XP Novice', 'Awarded for earning your first 100 XP.', 'xp_novice_badge.png', 100),
+
+                                                                                      ('0ac94993-a4d0-4874-8c6b-28325582744c', 'XP Champion', 'Awarded for earning 500 XP in total.', 'xp_champion_badge.png', 500),
+
+                                                                                      ('0cecd843-b23c-4a6f-a122-7073e1eab74d', 'XP Overlord', 'Awarded for earning 5000 XP in total.', 'xp_overlord_badge.png', 5000),
+
+                                                                                      ('7f83a68a-1a16-40ca-9369-d2ce3c49ed38', '7-Day Streak Achievement', 'Awarded when a user completes a habit for 7 consecutive days.', '7_day_streak_achievement.png', 50),
+
+                                                                                      ('27f7ffeb-59b7-4b19-84cd-4eb5edf166d1', 'Level 10 Reached', 'Awarded when a user reaches level 10.', 'level_10_badge.png', 1000);
